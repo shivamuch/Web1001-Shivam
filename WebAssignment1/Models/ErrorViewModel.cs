1a43d67e7462da16eee95cdb3fb8a3303c3f7b0c
Shivam Bajaj <shivam.bajaj1609@gmail.com>
2022-10-19 19:13:17 -0400

    created a repository

---
+++ WebAssignment1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace WebAssignment1.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
